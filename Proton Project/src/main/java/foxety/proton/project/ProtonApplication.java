92384bf74836da13a00c4adc9e0c31e54e5cbbbc
alexey <alexey@DESKTOP-IC87Q7U>
2019-10-01 16:57:19 +0300

    Structure step 1

--- Proton Project/src/main/java/foxety/proton/project/ProtonApplication.java
+++ Proton Project/src/main/java/foxety/proton/project/ProtonApplication.java
@@ -2,12 +2,19 @@ package foxety.proton.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @SpringBootApplication
 public class ProtonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProtonApplication.class , args); 
+		SpringApplication.run(ProtonApplication.class, args);
+	}
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer propertySources() {
+		return (new PropertySourcesPlaceholderConfigurer());
 	}
 
 }
